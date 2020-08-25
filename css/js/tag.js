95f1685237224586ddb4ed5cd0d39f26d706b4ef
Marlo Eichenberg Motta <marlo.eichenberg@gmail.com>
2020-08-25 12:36:49 -0300

    desafio: reescrever o objeto cores como uma 'function'

--- css/js/tag.js
+++ css/js/tag.js
@@ -17,16 +17,61 @@ const colors = {
     }
 }
 
+function getColorsFromArray(tagHtml) {
+    const tags = [
+        'p',
+        'div',
+        'span',
+        'section',
+        'ul',
+        'ol',
+        'header',
+        'nav',
+        'main',
+        'footer',
+        'form',
+        'body'    
+    ]
+
+    const colors = [
+        '#388e3c',
+        '#1565c0',
+        '#e53935',
+        '#f67809',
+        '#5e35b1',
+        '#fbc02d',
+        '#d81b60',
+        '#64dd17',
+        '#00acc1',
+        '#304ffe',
+        '#9f6581',
+        '#25b6da'
+    ]
+
+    let color = '#616161';
+    for (let i=0; i<tags.length; i++) {
+        if (tags[i] === tagHtml) {
+            color = colors[i]
+            break;
+        }
+    }
+    
+    return color;
+
+}
+
 
 document.querySelectorAll('.tag').forEach(e => {
     const tagName = e.tagName.toLowerCase()
 
-    e.style.borderColor = colors.get(tagName)
+    // e.style.borderColor = colors.get(tagName)
+    e.style.borderColor = getColorsFromArray(tagName)
 
     //renderiza o label apenas se não existir a classe '.nolabel'
     if (!e.classList.contains('nolabel')) {
         const label = document.createElement('label')
-        label.style.backgroundColor = colors.get(tagName)
+        // label.style.backgroundColor = colors.get(tagName)
+        label.style.backgroundColor = getColorsFromArray(tagName)
         label.innerHTML = tagName
         e.insertBefore(label, e.childNodes[0])
     }
